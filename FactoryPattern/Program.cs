bed55eb7243d044a025ffee1f4181905d44a74af
TylerN93 <tylernewton73@gmail.com>
2022-11-06 22:37:49 -0600

    completed

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace FactoryPattern
+﻿using System;
+
+namespace FactoryPattern
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numberOfTires;
+            bool input = false;
+
+            do
+            {
+                Console.WriteLine($"How many tires would you like for your vehicle to have?");
+
+                input = int.TryParse(Console.ReadLine(), out numberOfTires);
+            }
+            while (!input);
+
+            var vehicle = VehicleFactory.GetVehicle(numberOfTires);
+
+            vehicle.Drive();
+
+            
         }
     }
 }
---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int tires)
+        {
+           switch (tires)
+            {
+                case 2:
+                    return new Motorcycle();
+                case 4:
+                    return new Car();
+                default:
+                    return new Car();
+            }
+
+        }
+    }
+}
+
